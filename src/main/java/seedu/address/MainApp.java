5cbf8a958ae90873e181f86b2d3505bb776f5231
shaniceabigail <huangzhuxian@gmail.com>
2018-10-01 10:51:43 +0800

    updated initialisation and exit from AddressBook to 3VIA

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -58,7 +58,7 @@ public class MainApp extends Application {
 
     @Override
     public void init() throws Exception {
-        logger.info("=============================[ Initializing AddressBook ]===========================");
+        logger.info("=============================[ Initializing 3VIA app ]===========================");
         super.init();
 
         AppParameters appParameters = AppParameters.parse(getParameters());
@@ -194,7 +194,7 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        logger.info("Starting AddressBook " + MainApp.VERSION);
+        logger.info("Starting 3VIA app " + MainApp.VERSION);
         ui.start(primaryStage);
     }
 
