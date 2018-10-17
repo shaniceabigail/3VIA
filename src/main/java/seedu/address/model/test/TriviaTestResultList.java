9802333744947b087e7d43fa193ae8b913e93a9b
JunWei96 <kjunwei1996@gmail.com>
2018-10-17 09:17:08 +0800

    Add data structure to support efficient retrieval of attempts by card

--- src/main/java/seedu/address/model/test/TriviaTestResultList.java
+++ src/main/java/seedu/address/model/test/TriviaTestResultList.java
@@ -3,16 +3,22 @@ package seedu.address.model.test;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+
+import seedu.address.model.card.Card;
 
 /**
  * Contains the mapping of all the test.
  */
 public class TriviaTestResultList {
     private final List<TriviaTestResult> triviaTestResults;
+    private final HashMap<Card, List<Attempt>> attemptsOfCards;
 
     public TriviaTestResultList() {
         triviaTestResults = new ArrayList<>();
+        attemptsOfCards = new HashMap<>();
     }
 
     /**
@@ -20,10 +26,27 @@ public class TriviaTestResultList {
      */
     public void addTriviaTestResult(TriviaTestResult triviaTestResult) {
         requireAllNonNull(triviaTestResult);
+
         triviaTestResults.add(triviaTestResult);
+        mapCardsToAttempts(triviaTestResult.attempts);
     }
 
     public List<TriviaTestResult> getResultList() {
         return triviaTestResults;
     }
+
+    /**
+     * Will map cards to attempts for efficient retrieval of attempts by card.
+     */
+    private void mapCardsToAttempts(List<? extends Attempt> attempts) {
+        for (Attempt attempt : attempts) {
+            List<Attempt> existingAttemptsByCard = attemptsOfCards.get(attempt.attemptedCard);
+            if (existingAttemptsByCard == null) {
+                attemptsOfCards.put(attempt.attemptedCard, new ArrayList<>(Arrays.asList(attempt)));
+            } else {
+                existingAttemptsByCard.add(attempt);
+            }
+        }
+    }
+
 }
