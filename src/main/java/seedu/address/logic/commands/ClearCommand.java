4ad396466898af67299bc9606924b475fe12f892
Cary__Xx <e0140856@u.nus.edu>
2018-10-30 04:55:48 +0800

    Add support for clear command

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -3,8 +3,11 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 
 import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
+import seedu.address.model.TriviaBundle;
+import seedu.address.model.VersionedTriviaBundle;
 
 /**
  * Clears the address book.
@@ -12,14 +15,26 @@ import seedu.address.model.Model;
 public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
-    public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + " : Clears all the cards inside the card list.\n"
+            + " Parameters: null.\n"
+            + " Example: clear";
+
+    public static final String MESSAGE_SUCCESS = "Card list has been cleared!";
+
+    public static final String MESSAGE_NO_CARDS = "No cards to be cleared";
 
     @Override
-    public CommandResult execute(Model model, CommandHistory history) {
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException{
         requireNonNull(model);
-        model.resetData(new AddressBook());
+
+        if(!model.canClearCardList()) {
+            throw new CommandException(MESSAGE_NO_CARDS);
+        }
+        model.resetData(new TriviaBundle());
         model.commitAddressBook();
+        model.commitTriviaBundle();
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
