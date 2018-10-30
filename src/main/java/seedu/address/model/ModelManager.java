f3fa76f228c40f47a7da0c113a5883b1df4c5f41
JunWei96 <kjunwei1996@gmail.com>
2018-10-30 23:28:47 +0800

    Fix style error

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -143,16 +143,10 @@ public class ModelManager extends ComponentManager implements Model {
         indicateTriviaBundleChanged();
     }
 
-
     @Override
     public boolean canClearCardList() {
         return versionedTriviaBundle.canClearCardList();
     }
-
-    //@Override
-    //public void clearCardList() {
-    //    versionedTriviaBundle.resetData();
-    //}
 
     @Override
     public void updateCard(Card target, Card editedCard) {
