53bb6083745591faa39660855765f2e7f214cfcd
Cary__Xx <e0140856@u.nus.edu>
2018-10-30 22:33:19 +0800

    Edit clear command

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -162,6 +162,7 @@ public class AddCommandTest {
         public boolean canClearCardList() {
             throw new AssertionError("This method should not be called.");
         }
+
         @Override
         public void updateCard(Card target, Card editedCard) {
             throw new AssertionError("This method should not be called.");
--- src/test/java/seedu/address/logic/commands/ClearCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearCommandTest.java
@@ -7,9 +7,9 @@ import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 import org.junit.Test;
 
 import seedu.address.logic.CommandHistory;
-import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
+import seedu.address.model.TriviaBundle;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.test.TriviaResults;
 
@@ -18,24 +18,23 @@ public class ClearCommandTest {
     private CommandHistory commandHistory = new CommandHistory();
 
     @Test
-    public void execute_emptyAddressBook_success() {
+    public void execute_emptyTriviaBundle_success() {
         Model model = new ModelManager();
         Model expectedModel = new ModelManager();
-        expectedModel.commitAddressBook();
+        expectedModel.commitTriviaBundle();
 
         assertCommandSuccess(new ClearCommand(), model, commandHistory, ClearCommand.MESSAGE_SUCCESS, expectedModel);
     }
 
     @Test
-    public void execute_nonEmptyAddressBook_success() {
+    public void execute_nonEmptyTriviaBundle_success() {
         Model model = new ModelManager(getTypicalAddressBook(), getTypicalTriviaBundle(),
                 new TriviaResults(), new UserPrefs());
         Model expectedModel = new ModelManager(getTypicalAddressBook(), getTypicalTriviaBundle(),
                 new TriviaResults(), new UserPrefs());
-        expectedModel.resetData(new AddressBook());
-        expectedModel.commitAddressBook();
+        expectedModel.resetData(new TriviaBundle());
+        expectedModel.commitTriviaBundle();
 
         assertCommandSuccess(new ClearCommand(), model, commandHistory, ClearCommand.MESSAGE_SUCCESS, expectedModel);
     }
-
 }
