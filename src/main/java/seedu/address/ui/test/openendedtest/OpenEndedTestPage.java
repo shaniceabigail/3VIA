0c654b60069b861ae3534be1401d247d800fcfd1
junhui2778 <junhui2778@gmail.com>
2018-10-30 18:04:12 +0800

    added dummy FXML pages

--- src/main/java/seedu/address/ui/test/openendedtest/OpenEndedTestPage.java
+++ src/main/java/seedu/address/ui/test/openendedtest/OpenEndedTestPage.java
@@ -6,9 +6,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import seedu.address.commons.core.LogsCenter;
-import seedu.address.model.test.matchtest.MatchTest;
+import seedu.address.model.test.openendedtest.OpenEndedTest;
 import seedu.address.ui.test.TriviaTestPage;
-import seedu.address.ui.test.matchtest.AnswerListPanel;
 import seedu.address.ui.test.matchtest.QuestionListPanel;
 
 /**
@@ -33,17 +32,14 @@ public class OpenEndedTestPage extends TriviaTestPage {
     @FXML
     private Label matchTestDurationText;
 
-    public OpenEndedTestPage(MatchTest matchTest) {
+    public OpenEndedTestPage(OpenEndedTest openEndedTest) {
         super(FXML);
 
-        questionListPanel = new QuestionListPanel(matchTest.getQuestions());
+        //questionListPanel = new QuestionListPanel(openEndedTest.getQuestions());
         matchTestQuestionListPanelPlaceholder.getChildren().add(questionListPanel.getRoot());
 
-        answerListPanel = new AnswerListPanel(matchTest.getAnswers());
-        matchTestAnswerListPanelPlaceholder.getChildren().add(answerListPanel.getRoot());
-
-        matchTestTopicText.setText(matchTest.getTopic().topicName);
-        matchTestDurationText.textProperty().bind(matchTest.getDuration().asString());
+        matchTestTopicText.setText(openEndedTest.getTopic().topicName);
+        matchTestDurationText.textProperty().bind(openEndedTest.getDuration().asString());
 
         registerAsAnEventHandler(this);
     }
--- src/main/java/seedu/address/ui/test/openendedtest/OpenEndedTestResultPage.java
+++ src/main/java/seedu/address/ui/test/openendedtest/OpenEndedTestResultPage.java
@@ -1,4 +1,44 @@
 package seedu.address.ui.test.openendedtest;
 
-public class OpenEndedTestResultPage {
+import java.util.logging.Logger;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.util.DateUtil;
+import seedu.address.model.test.openendedtest.OpenEndedTest;
+import seedu.address.ui.test.TriviaTestResultPage;
+
+public class OpenEndedTestResultPage extends TriviaTestResultPage {
+    private static final String FXML = "test/openendedtest/OpenEndedTestResultPage.fxml";
+
+    private final Logger logger = LogsCenter.getLogger(getClass());
+    private final OpenEndedTest openEndedTest;
+
+    @javafx.fxml.FXML
+    private Label testDateText;
+
+    @FXML
+    private Label durationText;
+
+    @FXML
+    private Label topicText;
+
+    @FXML
+    private Label numOfCardsText;
+
+    @FXML
+    private StackPane attemptListPlaceholder;
+
+    public OpenEndedTestResultPage(OpenEndedTest openEndedTest) {
+        super(FXML);
+
+        this.openEndedTest = openEndedTest;
+        testDateText.setText(DateUtil.format(openEndedTest.getTestDate()));
+        durationText.setText(String.valueOf(openEndedTest.getDuration().getValue()) + "s");
+        topicText.setText(openEndedTest.getTopic().topicName);
+        numOfCardsText.setText(String.valueOf(openEndedTest.getCardsTested().size()));
+
+    }
 }
