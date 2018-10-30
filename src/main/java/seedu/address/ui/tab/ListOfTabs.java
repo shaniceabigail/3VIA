12d4c9bf3a7baab9a0f3355f6b7cb9faa6e9eb1f
shaniceabigail <huangzhuxian@gmail.com>
2018-10-30 16:31:02 +0800

    location updated into UI

--- src/main/java/seedu/address/ui/tab/ListOfTabs.java
+++ src/main/java/seedu/address/ui/tab/ListOfTabs.java
@@ -6,10 +6,10 @@ public class ListOfTabs {
     public static final String MESSAGE_TAB_CONSTRAINTS = "Tab has not been created";
     //public static final String TOPIC_VALIDATION_REGEX = "\\p{Alnum}+";
 
-    private ArrayList<Tab> _list;
+    private ArrayList<seedu.address.model.tab.Tab> _list;
 
     public ListOfTabs() {
-        _list = new ArrayList<Tab>();
+        _list = new ArrayList<seedu.address.model.tab.Tab>();
     }
 
     @Override
--- src/main/java/seedu/address/ui/tab/Tab.java
+++ src/main/java/seedu/address/ui/tab/Tab.java
@@ -6,11 +6,14 @@ import static java.util.Objects.requireNonNull;
  * Represents a tab in 3VIA.
  * Guarantees: immutable; name is valid as declared in {@link #isValidTabListed(String)}
  */
-public class Tab {
+public class Tab extends javafx.scene.control.Tab {
     private String _tabName;
 
     public Tab(String tabName) {
-        requireNonNull(tabName)
+        requireNonNull(tabName);
         _tabName = tabName;
     }
+
+    @Override
+    private
 }
