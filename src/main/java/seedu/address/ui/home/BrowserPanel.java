d4a711056a7775d1e67d9f5be1f6eeaddca718e3
JunWei96 <kjunwei1996@gmail.com>
2018-11-05 12:00:51 +0800

    Update Google Command

--- src/main/java/seedu/address/ui/home/BrowserPanel.java
+++ src/main/java/seedu/address/ui/home/BrowserPanel.java
@@ -1,6 +1,9 @@
 package seedu.address.ui.home;
 
+import java.io.IOException;
+import java.net.InetAddress;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 import com.google.common.eventbus.Subscribe;
@@ -23,8 +26,10 @@ import seedu.address.ui.UiPart;
 public class BrowserPanel extends UiPart<Region> {
 
     public static final String DEFAULT_PAGE = "default.html";
-    public static final String SEARCH_PAGE_URL =
-            "https://www.google.com.sg/search?q=";
+    public static final String DEFAULT_SEARCH_PAGE_URL = "https://www.google.com.sg/search?q=";
+    public static final String SECONDARY_SEARCH_PAGE_URL = "https://duckduckgo.com/?q=";
+    public static final String NO_CONNECTION_PAGE = "no_connection.html";
+    public static final String WEB_ERROR_PAGE = "web_error.html";
 
     private static final String FXML = "home/BrowserPanel.fxml";
 
@@ -44,15 +49,38 @@ public class BrowserPanel extends UiPart<Region> {
     }
 
     private void loadPersonPage(Person person) {
-        loadPage(SEARCH_PAGE_URL + person.getName().fullName);
+        loadPage(DEFAULT_SEARCH_PAGE_URL + person.getName().fullName);
     }
 
+    /**
+     * Load the research page of the given card.
+     */
     public void loadCardPage(Card card) {
-        loadPage(SEARCH_PAGE_URL + card.getQuestion().value);
+        if (isConnectedToInternet()) {
+            String primaryUrl = DEFAULT_SEARCH_PAGE_URL + card.getQuestion().value;
+            String secondaryUrl = SECONDARY_SEARCH_PAGE_URL + card.getQuestion().value;
+            loadPage(primaryUrl, secondaryUrl);
+        } else {
+            URL noConnectionPage = MainApp.class.getResource(FXML_FILE_FOLDER + NO_CONNECTION_PAGE);
+            loadPage(noConnectionPage.toExternalForm());
+        }
     }
 
-    public void loadPage(String url) {
-        Platform.runLater(() -> browser.getEngine().load(url));
+    /**
+     * Loads the page with the given search param
+     */
+    public void loadPage(String ...urls) {
+        Platform.runLater(() -> {
+            for (String url : urls) {
+                browser.getEngine().load(url);
+                if (browser.getEngine().getLocation().equals(url)) {
+                    break;
+                }
+            }
+            if (!Arrays.asList(urls).contains(browser.getEngine().getLocation())) {
+                browser.getEngine().load(FXML_FILE_FOLDER + WEB_ERROR_PAGE);
+            }
+        });
     }
 
     /**
@@ -68,6 +96,23 @@ public class BrowserPanel extends UiPart<Region> {
      */
     public void freeResources() {
         browser = null;
+    }
+
+    /**
+     * Checks whether there is internet connection by pinging google.com.
+     * @return true if there is an internet connection, else return false.
+     */
+    private boolean isConnectedToInternet() {
+        try {
+            if (InetAddress.getByName("www.google.com").isReachable(500)) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (IOException ioe) {
+            logger.warning("The checking of internet connectivity was interrupted.");
+            return false;
+        }
     }
 
     @Subscribe
